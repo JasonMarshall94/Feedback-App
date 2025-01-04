dc160d059c35a7e2a0f88b26345203055ce49362
Jason Marshall <jasonmarshall1994@gmail.com>
2025-01-04 15:56:59 -0500

    added react-icons. Added delete feedback functionality

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,15 @@
+import { FaTimes } from "react-icons/fa";
 import PropTypes from "prop-types";
 
 import Card from "./shared/Card";
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
@@ -13,6 +17,7 @@ function FeedbackItem({ item }) {
 
 FeedbackItem.propTypes = {
   item: PropTypes.object.isRequired,
+  handleDelete: PropTypes.func,
 };
 
 export default FeedbackItem;
