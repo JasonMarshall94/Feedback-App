186459b062e3d6c3b262be447d98bd01c83748c7
Jason Marshall <jasonmarshall1994@gmail.com>
2025-01-09 18:19:56 -0500

    added text input validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,9 +5,24 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
-    setText(e.target.value);
+    const textValue = e.target.value;
+
+    if (textValue === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (textValue.trim().length < 10) {
+      setMessage("Your feedback must be at least 10 characters long");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
+    setText(textValue);
   };
 
   return (
@@ -22,8 +37,12 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
